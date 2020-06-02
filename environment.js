82720250270da923babbf4a5d1d96018ea1f4830
Gilmar Andrade <gilmar-andrade@outlook.com>
2020-06-02 12:58:57 -0300

    solucao temporaria caminho api

--- environment.js
+++ environment.js
@@ -1,8 +1,10 @@
 module.exports = {
+    //FIXIT por alguma razão as variaveis de ambiente não estao sendo lidas no servidor, talves eu pudesse ler no server e dar um jeito de mandar para o vue
     caminho: "dist", //Aqui será definido a pasta de saída onde contém o index.html e os outros arquivos.
     forcarHTTPS: false, //Defina para true se desejar que o redirecionamento para HTTPS seja forçado (é necessário certificado SSL ativo)
     port: process.env.PORT || 3000,
     node_env: process.env.NOMDE_ENV,
-    api: process.env.baseApiUrl,
+    // api: process.env.baseApiUrl,
+    api: 'http://api-frenteprevencaocovidrn-org-br.umbler.net',
     api_local: 'http://localhost:3000',
 }
